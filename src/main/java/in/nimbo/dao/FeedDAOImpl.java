0b150c68bc6d7bcf06887848d79c19ac6b1d5fdf
Amin Borjian <borjianamin98@outlook.com>
2019-07-01 12:03:20 +0430

    Add FeedDAOImpl

--- src/main/java/in/nimbo/dao/FeedDAOImpl.java
+++ src/main/java/in/nimbo/dao/FeedDAOImpl.java
@@ -2,15 +2,32 @@ package in.nimbo.dao;
 
 import com.rometools.rome.feed.synd.SyndEntry;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class FeedDAOImpl implements FeedDAO {
-    public FeedDAOImpl() {
+    private static final String url = "jdbc:mysql://localhost:3306/nimbo_rss?useSSL=false&useUnicode=yes&characterEncoding=UTF-8";
+    private static final String username = "root";
+    private static final String password = "";
 
+    private Connection connection;
+
+    public FeedDAOImpl() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Unable to connect to MySQL: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL driver not found");
+        }
     }
 
     @Override
     public List<SyndEntry> getFeeds(String title) {
+
         return null;
     }
 
---
+++ src/test/java/in/nimbo/dao/FeedDAOTest.java
@@ -0,0 +1,14 @@
+package in.nimbo.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FeedDAOTest
+{
+    @Test
+    public void testConnection()
+    {
+        FeedDAO feedDAO = new FeedDAOImpl();
+    }
+}
